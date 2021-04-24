2941add766dd7e14e3648ddadfac9b8693c64eae
Dingdong <bingjilin@me.com>
2021-04-24 16:19:11 +0800

    Add image tag

--- src/magBundle.js
+++ src/magBundle.js
@@ -13,7 +13,7 @@ function magBundle(bundle_div){
 
     var dataset_names = ["dataset1", "dataset2", "dataset3"];
     var images_names = [["01.jpg", "02.jpg", "03.jpg"], ["01.jpg"], ["02.jpg"]];
-    var exp_names = [["exp01", "exp02"], ["exp01"], ["exp_02"]];
+    var exp_names_list = [["exp01", "exp02"], ["exp01"], ["exp_02"]];
 
     this.dataset_list = bundle_div.querySelector(".title .dropdown-content"); // The dataset (at title) dropdown menu
     this.title = bundle_div.querySelector(".title").childNodes[0];
@@ -37,9 +37,9 @@ function magBundle(bundle_div){
     }
     // Initialize the exp list and the canvas
     //<li><i class="far fa-square"></i><i class="fas fa-check-square"></i>Star</li>
-    for (var i = 0; i < exp_names[0].length; i++){
+    for (var i = 0; i < exp_names_list[0].length; i++){
         // ---------- Initialize the exp list ----------
-        var text = exp_names[0][i];
+        var text = exp_names_list[0][i];
         var li = document.createElement("li");
 
         var e_i_1 = document.createElement("i");
@@ -54,11 +54,18 @@ function magBundle(bundle_div){
         // ---------- Initialize the canvas ----------
         var div_e = document.createElement("div");
         div_e.classList.add("input_div");
+
+        // Image tag on top of the display overall
+        var img_name_tag = document.createElement("div");
+        img_name_tag.classList.add("img_name_tag");
+        img_name_tag.innerHTML = exp_names_list[0][i] + ": "  + images_names[0][0];
+
+        // Display overall
         var input_e = Object.assign(document.createElement("input"), 
             {"type": "image", 
             "src": images_names[0][0]}
             )
-        div_e.appendChild(input_e);
+        div_e.append(img_name_tag, input_e);
         mag_div_e.appendChild(div_e);
     }
     magnifying_div.drawInputCanvas(); // Initialize input canvas
@@ -75,8 +82,10 @@ function magBundle(bundle_div){
                 // Change image src
                 image_index = img_index_now;
                 var input_canvas = mag_div_e.querySelectorAll("input");
+                var img_name_tags = mag_div_e.querySelectorAll(".img_name_tag");
                 for (var i = 0; i < input_canvas.length; i++){
                     input_canvas[i].src = images_names[dataset_index][img_index_now];
+                    img_name_tags[i].innerHTML = img_name_tags[i].innerHTML.split(":")[0] + ": " + images_names[dataset_index][img_index_now];
                 }
                 magnifying_div.reattachImageObj();
             }
