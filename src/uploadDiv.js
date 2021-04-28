ce9f56e187d5c18bf2a6e28393eaf0fffa914df5
santisy <bingjilin@me.com>
2021-04-28 22:24:11 +0800

    Further optimize the uploading logic and info log
    1. Further optimize the info function
    2. Now also constrain the window pop and relative logic together with
    different kinds of info window.
    
    Note: should actively considering how to sync and update key meta data
    when after uploading and updating info on the server.

--- src/uploadDiv.js
+++ src/uploadDiv.js
@@ -13,12 +13,40 @@ function uploadDiv(e){
         upload_info.style.setProperty("display", "none");
         dir_upload.value = '';
         upload_data = new FormData();
+        if (upload_info.classList.contains("error_info")){
+          upload_info.classList.toggle("error_info");
+        }
+        ok_button.style.setProperty("display", "inline-block");
+    }
+
+    function setInfo(info=String(""), error_flag=false){
+      // The Info
+      let p_e = upload_info.querySelector("p");
+      p_e.innerHTML = info;
+      if (error_flag){
+        ok_button.style.setProperty("display", "none");
+        if (!upload_info.classList.contains("error_info")){
+          upload_info.classList.toggle("error_info");
+        }
+      }
+      upload_info.style.setProperty("display", "block");
     }
 
     window.addEventListener("click", function(ev){
-        if (ev.target != upload_info && ev.target.parentNode != upload_info){reset();}
+        if (ev.target != upload_info && 
+          ev.target.parentNode != upload_info &&
+          ev.target != dir_upload
+          ){reset();}
         }
     )
+
+    dir_upload.addEventListener("click", function(e){
+      if (input_frame.value == ""){
+        let info_now = "No " + "Dataset Name".bold() + " has been input!"
+        setInfo(info_now, true);
+        e.preventDefault();
+      }
+    })
 
     dir_upload.addEventListener("input", function(){
       //TODO: 
@@ -47,25 +75,32 @@ function uploadDiv(e){
       upload_data.append("exp_name", exp_name);
 
       // The Info
-      let p_e = upload_info.querySelector("p");
-      p_e.textContent = "Upload dataset: " + input_frame.value + ", exp_name: " 
-        + exp_name + " with " + upload_image_files.length + " images?";
-      upload_info.style.setProperty("display", "block");
+      if (upload_image_files.length > 0){
+        let info_now = "Upload dataset: " + input_frame.value.bold() + ", exp_name: " 
+          + exp_name.bold() + " with " + String(upload_image_files.length).bold() + " images?";
+        setInfo(info_now);
+      }
+      else {
+        let info_now = "No " + "qualified".bold() + 
+          " image selected! There must been image files (.jpg)" + 
+          " directly under the exp folder.";
+        setInfo(info_now, true);
+      }
       }
     )
 
     ok_button.addEventListener("click", function(){
-        const config = {
-          headers: { 'content-type': 'multipart/form-data' }
-        } 
-        axios.post('/upload/exp_images', upload_data, config).then(reset());
-        }
+      const config = {
+        headers: { 'content-type': 'multipart/form-data' }
+      } 
+      axios.post('/upload/exp_images', upload_data, config).then(reset());
+      }
     )
 
     cancel_button.addEventListener("click", function(){
-        console.log("Canceled")
-        reset();
-        }
+      console.log("Canceled")
+      reset();
+      }
     )
 }
 
