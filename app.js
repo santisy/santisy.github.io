65a0b63f9a5deb564f51f67cd90c1fa7900b690a
Dingdong <bingjilin@me.com>
2021-05-24 13:38:05 +0800

    latest commit of server end

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/mainStore', {useNewUrlParser: true, useUnifiedTopology: true});
 var multer  = require('multer')
 
 var upload = multer({ dest: 'uploads/' })
@@ -17,12 +19,20 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
 
+const ExpScheme = new mongoose.Schema({'exp_name': String, 'img_paths': [String]});
+const DatasetScheme = new mongoose.Schema({ 'dataset_name': String, 'exps': [ExpScheme]}); 
+
 app.post('/upload/exp_images', upload.any(),
     function(req, res){
         //TODO: Checking exp+dataset replica in dataset
         //TODO: How to determine the image names? 
         //  Everytime a document inserted we should traverse all
         //  through the collection?
+        let user_name = req.body["user_name"];
+        let dataset_name = req.body["dataset_name"];
+        let exp_name = req.body["exp_name"];
+        const model = mongoose.model(user_name, DatasetScheme);
+
         req.files.sort(function(a, b){
             let name_a = a["originalname"];
             let name_b = b["originalname"];
@@ -33,12 +43,32 @@ app.post('/upload/exp_images', upload.any(),
         )
         //console.log(req.files);
         let file_paths = new Array();
-        let img_names = new Array();
+        let img_names = new Array(); // Find the most common one or any other skills?
         req.files.map(file => file_paths.push(file["path"]));
+
+        const new_exp = new ExpScheme({'exp_name': exp_name, 'img_paths': file_paths});
+        // After pre-processing, we start to update the dataset
+        model.updateOne({'dataset_name': dataset_name}, 
+            {"$push": { "exps": new_exp} },
+            function (err, raw) {
+                if (err) return handleError(err);
+                console.log('The raw response from Mongo was ', raw);
+            }
+        );
+        model.findOne({ 'dataset_name': dataset_name, 'exps.exp_name': exp_name}, 
+            'name occupation', 
+            function (err, x) {
+            if (err) return handleError(err);
+
+            // update an existing dataset with a new exp
+            // TODO: Then update an array value
+            
+        });
+
         res.status(200).json(
-            {"dataset_name": req.body["dataset_name"], 
+            {"dataset_name": dataset_name, 
              "img_file_paths": file_paths,
-             "exp_name": req.body["exp_name"], 
+             "exp_name": exp_name, 
              "img_names": img_names,
             }
         );
